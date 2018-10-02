ee0c3bcceaf8e3b2147fb9f6c218e0da1b7e9688
Jimmy Martin Henriksen <jimm1034@edu.eal.dk>
2018-10-02 13:38:10 +0200

    Add project files.

---
+++ SmartMenuLibrary/Binding.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Text.RegularExpressions;
+using FunctionLibrary;
+
+namespace Binding
+{
+    public class ShowMenu
+    {
+        public string Something { get; private set; }
+
+
+        //Modtag input fra user i smartmenu klassen og hent metode fra funktions klassen
+        public void MenuOne()
+        {
+            Console.WriteLine(Functions.DoThis());//værdi 1 
+        }
+        internal void MenuTwo()
+        {
+            Console.WriteLine(Functions.DoThat());//værdi 2 
+        }
+        internal void MenuThree()
+        {
+            Console.WriteLine("Error");//værdi 3
+        }
+        internal void MenuFour()
+        {
+            Console.WriteLine(Functions.GetTheAnswerToLifeTheUniverseAndEverything());//værdi 4
+        }
+        internal void MenuEnglish()
+        {
+            //Engelsk menu
+            string[] lines = System.IO.File.ReadAllLines(@"..\..\MenuSpecDA.txt");
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+        internal void ExitMenu()
+        {
+            Console.WriteLine("Virker ikke helt endnu");
+        }
+    }
+}
---
+++ SmartMenuLibrary/SmartMenu.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+using Binding;
+using System.Collections;
+
+namespace SmartMenuLibrary
+{
+    public class SmartMenu
+    {
+        public void LoadMenu(string path)
+        {
+            string[] lines = System.IO.File.ReadAllLines(@"..\..\MenuSpecDA.txt");
+            foreach (string line in lines)
+            {
+                Console.WriteLine(line);
+            }
+        }
+        public void Activate()
+        {
+            {
+                string result = Console.ReadLine();
+
+                if (result.Equals("1"))
+                {
+                    Console.Clear();
+                    //result from MenuOne
+                    ShowMenu One = new ShowMenu();
+                    One.MenuOne();
+
+                }
+                else if (result.Equals("2"))
+                {
+                    Console.Clear();
+                    //Result from MenuTwo
+                    ShowMenu Two = new ShowMenu();
+                    Two.MenuTwo();
+                }
+                else if (result.Equals("3"))
+                {
+                    Console.Clear();
+                    //result from MenuThree BROKEN
+                    ShowMenu Three = new ShowMenu();
+                    Three.MenuThree();
+                }
+                else if (result.Equals("4"))
+                {
+                    Console.Clear();
+                    //result from MenuFour
+                    ShowMenu Four = new ShowMenu();
+                    Four.MenuFour();
+                }
+                else if (result.Equals("0"))
+                {
+                    //Afslut programmet
+                    ShowMenu exit = new ShowMenu();
+                    exit.ExitMenu();
+                }
+
+                if (result.Equals("9"))
+                {
+                    Console.Clear();
+                    //Skift til engelsk menu
+                    string[] lines2 = System.IO.File.ReadAllLines(@"..\..\MenuSpecEN.txt");
+                    foreach (string line in lines2)
+                    {
+                        Console.WriteLine(line);
+
+                    }
+                    SmartMenu english = new SmartMenu();
+                    english.Activate();
+
+                }
+            }
+
+        }
+  
+
+    }
+}
