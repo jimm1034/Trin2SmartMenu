6e30dd26a5cf7607c9563f5658bdb13dcacc9533
Muhammed Ali Akbuga <muha0373@edu.eal.dk>
2018-10-03 09:17:42 +0200

    Tilføjet og omdøbt test

---
+++ TestVersion1.cs/TestVersion1.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using FunctionLibrary;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SmartMenuLibrary;
+
+namespace TestVersion1.cs
+{
+    [TestClass]
+    public class TestVersion1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("Result from DoThis", Functions.DoThis());
+        }
+        [TestMethod]
+        public void TestMethod2()
+        {
+            Assert.AreEqual("Result from DoThat", Functions.DoThat());
+        }
+        [TestMethod]
+        public void TestMethod3()
+        {
+            Assert.AreEqual("Result from doing: Something", Functions.DoSomething("Something"));
+        }
+        [TestMethod]
+        public void TestMethod4()
+        {
+            Assert.AreEqual(42, Functions.GetTheAnswerToLifeTheUniverseAndEverything());
+        }
+    }
+}
